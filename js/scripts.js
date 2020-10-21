382ff441d083b00b2adea15c805aface5fe04ffa
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-21 11:33:41 -0700

    add skeleton structure to scripts.js; add beginning of form to index.html
      Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#make-dem-shouts").submit(function() {
+    const [declared value] = $("input[#this on the page]").val();
+
+    $(".[class]").text([declared value]);
+
+    $(#["the shouting"]).show();
+
+    event.preventDefault();
+  });
+});
